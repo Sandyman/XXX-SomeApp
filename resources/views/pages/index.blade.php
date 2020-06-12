b42da1c55d17cb601143981685f213e6075fd53b
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2020-06-12 16:09:29 +0800

    Minor App template change, add fixed footer
    Footer added, added Fira Code to styles, added account details for demo.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,14 +7,17 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-4">
+            <div class="col-md">
                 <div class="card">
                     <h1 class="card-header bg-primary text-light">Welcome</h1>
                     <div class="card-body">
-                        <p>Welcome to .</p>
-                        <p>This is a demonstration application for use by students in deployment
-                            testing. Deployment targets must meet the requirements for the
-                            application, or be configurable with ease.
+                        <p>Welcome to <strong class="text-primary">
+                                {{ config('app.name', 'Laravel') }}
+                            </strong>.</p>
+                        <p>
+                            This is a demonstration application for use by Diploma of
+                            Software Development (and other) students for testing the
+                            deployment of a web based application and API to the cloud.
                         </p>
                         <p>This application contains the following features:</p>
                         <ul>
@@ -29,7 +32,7 @@
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md">
                 <div class="card">
                     <h1 class="card-header bg-secondary text-light">
                         <small>Requirements</small>
@@ -38,22 +41,17 @@
                         <p>This application is built using or requires:</p>
                         <ul>
                             <li>PHP 7.3+</li>
-                            <li>MariaDB 10.2+ / MySql 8</li>
-                            <li>Laravel 7</li>
+                            <li>MariaDB 10.2+ / MySql 8+</li>
+                            <li>Laravel 7+</li>
                             <li>NPM</li>
-                            <li>Composer</li>
-                            <li>Composer</li>
-                            <li>Composer</li>
-                            <li>Composer</li>
+                            <li>Apache 2.4+</li>
+                            <li>...</li>
                         </ul>
                         <p>More details in the about page.</p>
                     </div>
                 </div>
             </div>
 
-                <div class="col-md-4">
-                    {{ $quote }}
-                </div>
 
         </div>
     </div>
