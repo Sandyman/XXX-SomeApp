f8a307aa75e79e214cb64b377a2d21072395f7d9
Sander Huijsen <sander.huijsen@nmtafe.wa.edu.au>
2020-06-24 16:20:11 +0800

    Changed colour scheme.

--- public/js/app.js
+++ public/js/app.js
@@ -37885,8 +37885,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\l7-api-passport\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\l7-api-passport\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\sh-ceo-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\sh-ceo-app\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
